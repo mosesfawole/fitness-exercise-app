9a12917d71b82c8cdb1c6347b35d39431e9da226
Moses Fawole <mosesfawole2001@gmail.com>
2022-06-17 19:45:30 +0100

    Exercise detail completed

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
-import { exerciseOptions, fetchData } from "../pages/utils/fetchData";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [bodyParts, setBodyParts] = useState([]);
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,8 +6,8 @@ export const exerciseOptions = {
   },
 };
 export const fetchData = async (url, options) => {
-  const response = await fetch(url, options);
-  const data = await response.json();
+  const res = await fetch(url, options);
+  const data = await res.json();
 
   return data;
 };
